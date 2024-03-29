974468edb807f70127c494f0effc66d5c70176c3
AVXQT001 <136900110+AVXQT001@users.noreply.github.com>
2024-03-29 19:36:50 +0530

    Update Cloud_Codes.py

--- Cloud_Codes.py
+++ Cloud_Codes.py
@@ -2,4 +2,12 @@ import os, datetime
 
 def info():                                                                                                                        
   print("Welcome to Cloud Imports!")
-  print(os.getcwd())
+  
+def is_prime(num: int):
+    for x in range(2, num):
+        if num % x == 0:
+            return False
+    return True
+
+def prime(n):                                        
+  return filter(is_prime, n)
