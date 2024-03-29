903458801413c330bcb784868b251a43cd1dd9f3
AVXQT001 <136900110+AVXQT001@users.noreply.github.com>
2024-03-29 19:44:30 +0530

    Update Cloud_Codes.py

--- Cloud_Codes.py
+++ Cloud_Codes.py
@@ -12,10 +12,10 @@ def is_prime(num: int):
 class prime:
   def __init__(self, n):
     self.n = n
-    ans = filter(is_prime, range(1, self.n+1))
+    self.ans = filter(is_prime, range(1, self.n+1))
     
   def __repr__(self):
-    return ans               
+    return self.ans               
 
   def __str__(self):
-    return str(list(ans))
+    return str(list(self.ans))
